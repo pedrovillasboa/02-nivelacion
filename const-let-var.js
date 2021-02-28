908947848f07d1c440020d55baf128a126bcef56
pedro.villasboa <pedro.villasboa@gmail.com>
2021-02-27 23:42:48 -0300

    caambios      02

---
+++ const-let-var.js
@@ -0,0 +1,16 @@
+let  nombre ='pedro';
+
+if(true){
+    let nombre = 'luis';
+
+
+} 
+console.log(nombre);
+
+
+const nombre = 'Betty';
+
+if(true){
+    const nombre = 'Pedro';
+}
+console.log(nombre);
