33be8dd983689e95ba21bff0f517295d5c7f4c57
pedro.villasboa <pedro.villasboa@gmail.com>
2021-03-03 18:13:49 -0300

    cambios

---
+++ flecha.js
@@ -0,0 +1,13 @@
+//*function sumar (a , b){
+    return a + b;
+
+}*/
+
+//*function saludar (nombre){
+   // return `hola ${nombre}`;
+}
+//const sumar =(a , b )=> a+b;
+const saludar=(nombre)=> `Hola ${nombre}`;
+
+console.log(saludar('Pedro'));
+//console.log(sumar(10,20));
---
+++ template-strings.js
@@ -0,0 +1,5 @@
+const nombre = 'Pepe';
+const real = ' Guillermo';
+const salida = nombre + '' + real;
+
+console.log(salida);
