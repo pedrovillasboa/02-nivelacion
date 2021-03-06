e65b9f1b17e1b85c07f5d34840439c9cf3bacdab
pedro.villasboa <pedro.villasboa@gmail.com>
2021-03-06 17:54:40 -0300

    cambios

--- callback-hell.js
+++ callback-hell.js
@@ -24,7 +24,7 @@ const sueldos =[
         id: 2,
         sueldo: 2000
 
-    },
+    }
    
 ]
 const getEmpleadosById = (id ,callback) => {
@@ -33,7 +33,31 @@ const getEmpleadosById = (id ,callback) => {
       callback(empleado);
  }
 }
+const getSueldoById = (id, callback) =>{
+    const sueldo = sueldos.find((s ) =>s.id ===id );
+    if (sueldo){
+        callback(null,sueldo);
+        return;
+    }
+    callback(`El sueldo con id ${id}no existe`);
+}
 const id = 1;
 
 getEmpleadosById(id,({nombre})=>{
     cosole.log(`el empleado: ${nombre}...`;)
+
+
+    const getEmpleadoById = async (id) => {
+        try {
+          const empleado = empleados.find((e) => e.id === id);
+      
+          if (empleado) {
+            return empleado;
+          }
+          throw new Error("El empleado no existe");
+        } catch (error) {
+          throw error;
+        }
+      }
+
+   
